056e6ef23ffc8bc6cb1023f273ee350b54daefa7
cys24 <yoktoke@gmail.com>
2022-08-12 15:10:14 +0900

    Enter

--- pj_world_cup/settings.py
+++ pj_world_cup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*)_f0*722-f!*ueojo6t2h4x9$_&2h)n4oq#bx%aro^w5ys5qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
